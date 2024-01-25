c2730f800458d986bff72af64115b5605ba89f21
RoaaK95 <ruaakhuldoon.26@gmail.com>
2024-01-25 07:08:41 +0300

    Graph commit
    - Initializing lists of Nodes, Edges,Pathlist
    -Find Node method
    -Get Path point method

--- Assets/Scripts/Graph/Graph.cs
+++ Assets/Scripts/Graph/Graph.cs
@@ -4,4 +4,44 @@ using UnityEngine;
 
 public class Graph
 {
+    private List<Node> nodes = new List<Node>();
+    public List<Node> pathList = new List<Node>();
+    private List<Edge> edges = new List<Edge>();
+
+    public Graph()
+    {
+
+    }
+    public void AddNode(GameObject id)
+    {
+        Node node = new Node(id);
+        nodes.Add(node);
+    }
+
+    public void AddEdge(GameObject fromNode, GameObject toNode)
+    {
+        Node from = FindNode(fromNode);
+        Node to = FindNode(toNode);
+
+        if (from != null && to != null)
+        {
+            Edge e = new Edge(from, to);
+            edges.Add(e);
+            from.edgeList.Add(e);
+        }
+    }
+
+    public GameObject GetPathPoint(int index)
+    {
+        return pathList[index].GetId();
+    }
+    private Node FindNode(GameObject id)
+    {
+        foreach (Node n in nodes)
+        {
+            if (n.GetId() == id)
+                return n;
+        }
+        return null;
+    }
 }
